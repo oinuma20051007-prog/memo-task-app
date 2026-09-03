81a02e190067d3d40ee941b1c692f3a1ac3ff481
oinuma20051007-prog <oinuma20051007@gmail.com>
2026-09-03 18:24:46 +0900

    Refactor includes and update stylesheet path

--- memos.php
+++ memos.php
@@ -1,12 +1,10 @@
 <?php
 
-include "db.php";
+require_once __DIR__ . "/config/db.php";
 
-
-/* 保存・更新処理 */
-include "genre_update.php";
-include "save.php";
-include "memo_update.php";
+include __DIR__ . "/actions/genre_update.php";
+include __DIR__ . "/actions/save.php";
+include __DIR__ . "/actions/memo_update.php";
 
 
 /* =========================
@@ -95,10 +93,10 @@ $genres =
 
     <title>メモ</title>
 
-    <link
-        rel="stylesheet"
-        href="style.css"
-    >
+ <link
+    rel="stylesheet"
+    href="assets/css/style.css"
+>
 
 </head>
 
@@ -654,7 +652,7 @@ $genres =
 
     <?php include "calendar.php"; ?>
 
-    <?php include "input_form.php"; ?>
+   <?php include __DIR__ . "/includes/input_form.php"; ?>
 
 
 </div>
